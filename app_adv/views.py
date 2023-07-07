c3c07270e5240ae6876557e89c2a9974d4d472bf
hypnotoad4 <zdima761@gmail.com>
2023-07-07 13:39:59 +0300

    123

--- app_adv/views.py
+++ app_adv/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("ALL GOOD!")
+    return HttpResponse("Hello, world. You're at the polls index.")
